a10096727be5bb8d17c88fab33ed43ff12a736e4
Jordan Wiens <jordan@psifertex.com>
2022-11-30 15:19:25 -0500

    allow emoji in exported plugins

--- __init__.py
+++ __init__.py
@@ -618,7 +618,7 @@ The above copyright notice and this permission notice shall be included in all c
 
 THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 '''
-        with open(os.path.join(candidate, "plugin.json"), 'w') as pluginjson:
+        with open(os.path.join(candidate, "plugin.json"), 'w', encoding='utf8') as pluginjson:
             #not using f strings 'cause json isn't great for that
             pluginjson.write('''{
   "pluginmetadataversion": 2,
@@ -661,7 +661,7 @@ THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMP
 
         #TODO: Optionally export plugin as UIPlugin with helpers established if
         #current_* appears anywhere in it
-        with open(os.path.join(candidate, "__init__.py"), 'w') as initpy:
+        with open(os.path.join(candidate, "__init__.py"), 'w', encoding='utf8') as initpy:
             if self.edit.toPlainText().count("\t") > self.edit.toPlainText().count("    "):
                 delim = "\t"
             else:
