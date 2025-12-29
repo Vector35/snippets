0fb694ae5b77233dc3bb07b50a79f773283883ec
Jordan Wiens <github@psifertex.com>
2025-12-29 12:48:27 -0500

    fix updator for new html format from github

--- update_example_snippets.py
+++ update_example_snippets.py
@@ -1,16 +1,11 @@
-# update snippets
-#
 # Automatically download and update this collection of snippets to your local snippet folder
 
 from zipfile import ZipFile
 from tempfile import TemporaryFile
 import os
 
-#TODO: Merge remote with local description or hotkey changes (AKA: if filename matches, skip the first two lines, truncate, re-write the rest)
-
 domain = b'https://gist.github.com'
 path = b'/psifertex/6fbc7532f536775194edd26290892ef7' # Feel free to adapt to your own setup
-archive = path + b'/archive/'
 subfolder = 'default'                                 # Change to save the snippets to a different sub-folder
 tab2space = False
 width = 4
@@ -33,10 +28,11 @@ def update_snippets():
     url = domain + path
     log_info("Downloading from: %s" % url)
     source = download(url)
-    zipPath = [s for s in source.split(b'\"') if s.endswith(b'.zip') and archive in s]
-    if len(zipPath) != 2:
-        log_error("Update failed.")
+    zipPath = [s for s in source.split(b'\"') if s.endswith(b'.zip') and b'/archive/' in s]
+    if len(zipPath) == 0:
+        log_error("Update failed: No archive ZIP found.")
         return
+    # Take the first match (there may be duplicates)
     url = domain + zipPath[0]
 
     log_info("Downloading from: %s" % url)
@@ -48,11 +44,36 @@ def update_snippets():
                 if item.filename[-1] == '/':
                     continue
                 basename = os.path.basename(item.filename)
-                with open(os.path.join(snippetPath, basename), 'wb') as f:
-                    if tab2space:
-                        f.write(zip.read(item).replace(b'\t', b' ' * width))
+                targetPath = os.path.join(snippetPath, basename)
+
+                # Read remote content
+                remote_content = zip.read(item)
+                if tab2space:
+                    remote_content = remote_content.replace(b'\t', b' ' * width)
+
+                # Merge with local if file exists (preserve first two lines)
+                if os.path.exists(targetPath):
+                    with open(targetPath, 'rb') as local_file:
+                        local_lines = local_file.readlines()
+
+                    # If local file has at least 2 lines, preserve them
+                    if len(local_lines) >= 2:
+                        remote_lines = remote_content.splitlines(keepends=True)
+                        if len(remote_lines) >= 2:
+                            # Keep first two lines from local, rest from remote
+                            merged_content = b''.join(local_lines[:2] + remote_lines[2:])
+                            log_info("Merging %s (preserving local description/hotkey)" % item.filename)
+                        else:
+                            # Remote file too short, just use it as-is
+                            merged_content = remote_content
                     else:
-                        f.write(zip.read(item))
+                        # Local file too short, just use remote
+                        merged_content = remote_content
+                else:
+                    merged_content = remote_content
+
+                with open(targetPath, 'wb') as f:
+                    f.write(merged_content)
                     log_info("Extracting %s" % item.filename)
 
 update_snippets()
